4f50195d26616263d8e2477e3fe17f35d70a9e85
thinh2000 <s3777196@rmit.edu.vn>
2022-04-03 22:08:47 +0700

    Add function to allow user import their own CSV file

--- assignment1/src/main/java/com/example/assignment1/Assignment1Application.java
+++ assignment1/src/main/java/com/example/assignment1/Assignment1Application.java
@@ -10,26 +10,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Assignment1Application {
+	static Scanner scanner = new Scanner(System.in);
+	static ListOfStudent students = new ListOfStudent();
+	static ListOfCourse courses = new ListOfCourse();
+	static String path = "";
+	static String line = "";
 
 	public static void main(String[] args) {
+		importCSVFile();
+	}
 
-		ListOfStudent students = new ListOfStudent();
-		ListOfCourse courses = new ListOfCourse();
-
-
-		String path = "/assignment1/assignment1/src/main/resources/default.csv";
-		String line = "";
-
+	public static void readDefaultCSV(){
+		path = "/assignment1/assignment1/src/main/resources/default.csv";
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(path));
 
 			while((line = br.readLine()) != null){
 				String[] values = line.split(",");
-				students.addData(values[0],values[1],values[2]);
-				courses.addCourse(values[3],values[4],values[5]);
+				students.addData(values[0].replace(" ",""),values[1].replace(" ",""),values[2].replace(" ",""));
+				courses.addCourse(values[3].replace(" ",""),values[4].replace(" ",""),values[5].replace(" ",""));
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
@@ -40,6 +43,38 @@ public class Assignment1Application {
 
 		mainMenu();
 	}
+
+	public static void readUserCSVFile(){
+		//the file must be in the same directory.
+		System.out.print("\tEnter PATH to your CSV file (ex: /assignment1/assignment1/src/main/resources/YourFile.csv): ");
+		path = scanner.nextLine();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+
+			while((line = br.readLine()) != null){
+				String[] values = line.split(",");
+				students.addData(values[0].replace(" ",""),values[1].replace(" ",""),values[2].replace(" ",""));
+				courses.addCourse(values[3].replace(" ",""),values[4].replace(" ",""),values[5].replace(" ",""));
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+
+		students.print(students.getAll());
+		courses.print(courses.getAll());
+
+		mainMenu();
+	}
+
+
+	public static void  importCSVFile(){
+		ControlPanel ChooseFile = new ControlPanel("Choose csv file to import");
+		ChooseFile.addOption("Use default CSV file",Assignment1Application::readDefaultCSV);
+		ChooseFile.addOption("Import your own CSV file",Assignment1Application::readUserCSVFile);
+		ChooseFile.start();
+	}
+
+
 	public static void mainMenu() {
 
 		ControlPanel MainMenu = new ControlPanel("Main Menu");
